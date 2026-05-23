b91b8d2cd98c9035a95aa46ac34efb4fd8026d6d
Artjom Vejsel <akawolf0@gmail.com>
2026-05-23 18:54:57 +0300

    users: drop smart-monitor SSH key + smartctl sudoers (scrutiny-collector pushes locally, not via SSH)

--- modules/users.nix
+++ modules/users.nix
@@ -7,15 +7,6 @@
   # Passwordless sudo for the wheel group.
   security.sudo.wheelNeedsPassword = false;
 
-  # Allow akawolf to run smartctl without password (used by remote SMART monitor).
-  security.sudo.extraRules = [{
-    users = [ "akawolf" ];
-    commands = [{
-      command = "${pkgs.smartmontools}/bin/smartctl";
-      options = [ "NOPASSWD" ];
-    }];
-  }];
-
   users.users.akawolf = {
     isNormalUser = true;
     uid = 1000;
@@ -24,9 +15,6 @@
     openssh.authorizedKeys.keys = [
       # Personal login key.
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC1UZUcjfpcxoG89bVvCM5XxDdgnd5bkkM8MaijEI9oT akawolf0@gmail.com"
-      # Restricted monitor key: forced command, no shell/pty/forwarding.
-      # Client passes "/dev/sdX" as ssh command; sshd executes only the wrapped smartctl.
-      ''restrict,command="sudo /run/current-system/sw/bin/smartctl --json -a $SSH_ORIGINAL_COMMAND" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFqtIwoJ94cyvT0nHgTI2ElSD5F2GyYiDEedh/pVYZ1u smart-monitor@akawolf.org''
     ];
   };
 }
